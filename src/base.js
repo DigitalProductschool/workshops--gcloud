cee72f9ace9cd1cb65372aed0ed6618ee480a4b2
Michael Stockerl <stockerl@unternehmertum.de>
2019-02-14 06:02:47 +0100

    Remove wrong credentials

--- src/base.js
+++ src/base.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 require('dotenv').config();
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCRMDtJfde3awuWoLHNZS5PdfyMdKN95Bk",
-  authDomain: "gcloud-intro.firebaseapp.com",
-  databaseURL: "https://gcloud-intro.firebaseio.com",
-  projectId: "gcloud-intro",
-  storageBucket: "gcloud-intro.appspot.com",
-  messagingSenderId: "905301150822"
+  apiKey: "the api key",
+  authDomain: "your auth domain here",
+  databaseURL: "your database url here",
+  projectId: "your project Id here",
+  storageBucket: "your storage bucket here",
+  messagingSenderId: "your messaging sender id here"
 });
 
 export default app;
